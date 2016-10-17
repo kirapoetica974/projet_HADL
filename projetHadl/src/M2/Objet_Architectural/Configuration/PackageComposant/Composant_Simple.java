a821f05dd6a9dcf70ebeb555a79a7a99f76523e6
Nina <megapepe@hotmail.fr>
2016-10-17 17:34:55 +0200

    Composants

--- projetHadl/src/M2/Objet_Architectural/Configuration/PackageComposant/Composant_Simple.java
+++ projetHadl/src/M2/Objet_Architectural/Configuration/PackageComposant/Composant_Simple.java
@@ -1,19 +1,6 @@
 package M2.Objet_Architectural.Configuration.PackageComposant;
 
-import M2.Objet_Architectural.Interface_Communication.Interface;
 
 public class Composant_Simple extends Composant {
-
-	private Interface interfaceCommunication;
-
-	@Override
-	public Interface getInterfaceCommunication() {
-		return interfaceCommunication;
-	}
-
-	@Override
-	public void setInterfaceCommunication(Interface interfaceCommunication) {
-		this.interfaceCommunication = interfaceCommunication;
-	}
 
 }
