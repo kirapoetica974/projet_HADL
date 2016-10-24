5be99d8616f2731cc0db63da51e9cf538c25e863
kirapoetica974 <retco.contact@gmail.com>
2016-10-24 17:39:14 +0200

    ajout client, simple cs etc

--- projetHadl/src/M1/Systeme_Simple_CS/Composant_Client/Send_Request.java
+++
@@ -1,7 +0,0 @@
-package M1.Systeme_Simple_CS.Composant_Client;
-
-import M2.Objet_Architectural.Interface_Communication.Port_Composant_Requis;
-
-public class Send_Request extends Port_Composant_Requis {
-
-}
--- projetHadl/src/M1/Systeme_Simple_CS/Simple_CS.java
+++ projetHadl/src/M1/Systeme_Simple_CS/Simple_CS.java
@@ -1,7 +1,16 @@
 package M1.Systeme_Simple_CS;
 
+import M1.Systeme_Simple_CS.Composant_Client.Client;
 import M2.Objet_Architectural.Configuration.Configuration;
 
 public class Simple_CS extends Configuration {
 
+	private Client client;
+
+	public Simple_CS(Client client) {
+		this.client = client;
+		this.setNom("Simple CS");
+
+		this.addObjetArchitectural(client);
+	}
 }
