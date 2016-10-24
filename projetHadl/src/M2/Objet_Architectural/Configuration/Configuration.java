2fd932211220c5e4871538f6ac76f0456addcd6c
Nina <megapepe@hotmail.fr>
2016-10-24 17:27:38 +0200

    get et set objets architecturaux dans configuration

--- projetHadl/src/M2/Objet_Architectural/Configuration/Configuration.java
+++ projetHadl/src/M2/Objet_Architectural/Configuration/Configuration.java
@@ -12,4 +12,35 @@ public class Configuration extends Objet_Architectural {
 
 	List<Lien_Binding> listeLienBinding = new ArrayList<Lien_Binding>();
 
+	/**
+	 * @return listeObjetArchitectural
+	 */
+	public List<Objet_Architectural> getListeObjetArchitectural() {
+		return listeObjetArchitectural;
+	}
+
+	/**
+	 * @param listeObjetArchitectural
+	 *            la liste des objets architecturaux à modifier
+	 */
+	public void setListeObjetArchitectural(
+			List<Objet_Architectural> listeObjetArchitectural) {
+		this.listeObjetArchitectural = listeObjetArchitectural;
+	}
+
+	/**
+	 * @return listeLienBinding
+	 */
+	public List<Lien_Binding> getListeLienBinding() {
+		return listeLienBinding;
+	}
+
+	/**
+	 * @param listeLienBinding
+	 *            la liste des liens binding à modifier
+	 */
+	public void setListeLienBinding(List<Lien_Binding> listeLienBinding) {
+		this.listeLienBinding = listeLienBinding;
+	}
+
 }
