837430d0f94625d680dde0e0fb322fbdcaaebbe6
kirapoetica974 <retco.contact@gmail.com>
2016-10-24 14:28:27 +0200

    modification composant simple

--- projetHadl/src/M2/Objet_Architectural/Configuration/PackageComposant/Composant_Simple.java
+++ projetHadl/src/M2/Objet_Architectural/Configuration/PackageComposant/Composant_Simple.java
@@ -1,6 +1,42 @@
 package M2.Objet_Architectural.Configuration.PackageComposant;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Composant_Simple extends Composant {
+
+	private List<Propriete> listePropriete = new ArrayList<Propriete>();
+
+	private List<Contrainte> listeContrainte = new ArrayList<Contrainte>();
+
+	/**
+	 * @return la listePropriete
+	 */
+	public List<Propriete> getListePropriete() {
+		return listePropriete;
+	}
+
+	/**
+	 * @param listePropriete
+	 *            la listePropriete à modifier
+	 */
+	public void setListePropriete(List<Propriete> listePropriete) {
+		this.listePropriete = listePropriete;
+	}
+
+	/**
+	 * @return la listeContrainte
+	 */
+	public List<Contrainte> getListeContrainte() {
+		return listeContrainte;
+	}
+
+	/**
+	 * @param listeContrainte
+	 *            la listeContrainte à modifier
+	 */
+	public void setListeContrainte(List<Contrainte> listeContrainte) {
+		this.listeContrainte = listeContrainte;
+	}
 
 }
